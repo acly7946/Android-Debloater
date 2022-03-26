dce962520ff6fe9b124b43cd87a27c53ad38b66f
acly7946 <66972514+acly7946@users.noreply.github.com>
2022-03-26 12:51:47 +0000

    get common name using aapt

--- debloat.sh
+++ debloat.sh
@@ -4,12 +4,25 @@
 
 install()
 {
+
 	ERROR=$(adb shell cmd package install-existing "${1}" 2>&1)
 	RETURN=$?
 	if [ ${RETURN} != 0 ]; then
 		printf "\r%sFAILED %s${1} %s${ERROR}\n" "$(tput setaf 1)" "$(tput sgr0)" "$(tput setaf 3)"
 	else
-		printf "\r%sSUCCESS ${1}\n%s" "$(tput setaf 2)" "$(tput sgr0)"
+		APKPATH=$(adb shell pm path "${1}" 2>&1 | cut -f 2 -d ":")
+		if [ -z "${APKPATH}" ]; then
+			printf "\r%sSKIPPED %s${1} \n" "$(tput setaf 4)" "$(tput sgr0)"
+			return
+		fi
+
+		# Get common name
+		NAME=$(adb shell /data/local/tmp/aapt-arm-pie d badging "${APKPATH}" 2>&1 | grep "application-label:" | cut -f 2 -d ":")
+		if [ -z "${NAME}" ]; then # Set to package name if no common name
+			NAME="${1}"
+			return
+		fi
+		printf "\r%sSUCCESS %s${NAME}\n" "$(tput setaf 2)" "$(tput sgr0)"
 	fi
 }
 
