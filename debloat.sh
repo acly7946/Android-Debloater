1015af7d2097737e5a313f6dd71588a15fa70a8f
acly7946 <66972514+acly7946@users.noreply.github.com>
2022-03-26 12:47:38 +0000

    get common name using aapt

--- debloat.sh
+++ debloat.sh
@@ -15,22 +15,40 @@ install()
 
 uninstall()
 {
+	APKPATH=$(adb shell pm path "${1}" 2>&1 | cut -f 2 -d ":")
+	if [ -z "${APKPATH}" ]; then
+		printf "\r%sSKIPPED %s${1} \n" "$(tput setaf 4)" "$(tput sgr0)"
+		return
+	fi
+
+	# Get common name
+	NAME=$(adb shell /data/local/tmp/aapt-arm-pie d badging "${APKPATH}" 2>&1 | grep "application-label:" | cut -f 2 -d ":")
+	if [ -z "${NAME}" ]; then # Set to package name if no common name
+		NAME="${1}"
+		return
+	fi
+
 	ERROR=$(adb shell pm uninstall -k --user 0 "${1}" 2>&1)
 	RETURN=$?
 	if [ ${RETURN} != 0 ]; then
-		printf "\r%sFAILED %s${1} %s${ERROR}\n" "$(tput setaf 1)" "$(tput sgr0)" "$(tput setaf 3)"
+		printf "\r%sFAILED %s${NAME} %s${ERROR}\n" "$(tput setaf 1)" "$(tput sgr0)" "$(tput setaf 3)"
 	else
-		printf "\r%sSUCCESS ${1}\n%s" "$(tput setaf 2)" "$(tput sgr0)"
+		printf "\r%sSUCCESS %s${NAME}\n" "$(tput setaf 2)" "$(tput sgr0)"
 	fi
 }
 
 BASENAME=$(basename "${0}")
-
 adb start-server # initialise adb first
+adb push aapt-arm-pie /data/local/tmp >/dev/null
+adb shell chmod 0755 /data/local/tmp/aapt-arm-pie
+
+# Check if files were specified
 if [ "$#" = 0 ]; then
 	echo "Usage: ${0} <INPUTFILE> <INPUTFILE2>"
 	exit 1
 fi
+
+# Either debloat or rebloat or fail
 if [ "${BASENAME}" = "debloat.sh" ]; then
 	echo "Uninstalling..."
 	while IFS= read -r PACKAGE; do
@@ -45,4 +63,6 @@ else
 	echo "File must be called 'debloat.sh' or 'rebloat.sh'"
 	exit 1
 fi
+
 wait
+adb shell rm /data/local/tmp/aapt-arm-pie
